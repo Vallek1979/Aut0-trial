407aa8dbfeea481a7b02125b02cca8af3bfd86a7
unknown <jayr1271@hotmail.co.uk>
2020-03-23 09:09:06 +0000

    Added a Procfile

--- 01-login/public/js/app.js
+++ 01-login/public/js/app.js
@@ -39,7 +39,7 @@ const logout = () => {
 /**
  * Retrieves the auth configuration from the server
  */
-const fetchAuthConfig = () => fetch("auth_config.json");
+const fetchAuthConfig = () => fetch("auth_config.js");
 
 /**
  * Initializes the Auth0 client
